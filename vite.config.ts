ba7deff8814136496518ccc4a148334a814c7eb5
majid-the-ark <majid.onaisy@thearknetworks.com>
2026-02-05 21:40:05 +0200

    vercel settings update for deployment v2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     server: {
